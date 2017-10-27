1107fce138ff100502da8e5a0c98ecc1317cbcca
Camilo Alarcón Romero <camilo.park@gmail.com>
2017-10-27 13:33:39 -0300

    dirección omg corregida

--- FashionProject2017_(GIT)/src/Vista/formCliente.java
+++ FashionProject2017_(GIT)/src/Vista/formCliente.java
@@ -394,7 +394,7 @@ public class formCliente extends javax.swing.JFrame {
 
         jLabel14.setText("*");
 
-        btnguardar.setIcon(new javax.swing.ImageIcon("/Users/alexndr/GitHub/FashionProject_INFO264_2017/FashionProject2017_(GIT)/files/modificar.png")); // NOI18N
+        btnguardar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Files/modificar.png"))); // NOI18N
         btnguardar.setText("Guardar");
         btnguardar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btnguardar.addActionListener(new java.awt.event.ActionListener() {
@@ -403,7 +403,7 @@ public class formCliente extends javax.swing.JFrame {
             }
         });
 
-        btncancelar.setIcon(new javax.swing.ImageIcon("/Users/alexndr/GitHub/FashionProject_INFO264_2017/FashionProject2017_(GIT)/files/cancel_77947.png")); // NOI18N
+        btncancelar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Files/cancel_77947.png"))); // NOI18N
         btncancelar.setText("Cancelar");
         btncancelar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btncancelar.addActionListener(new java.awt.event.ActionListener() {
@@ -597,7 +597,7 @@ public class formCliente extends javax.swing.JFrame {
                 .addComponent(lblregistrostotal))
         );
 
-        btnsalir.setIcon(new javax.swing.ImageIcon("/Users/alexndr/GitHub/FashionProject_INFO264_2017/FashionProject2017_(GIT)/files/atras.png")); // NOI18N
+        btnsalir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Files/atras.png"))); // NOI18N
         btnsalir.setText("Volver al Menú");
         btnsalir.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btnsalir.addActionListener(new java.awt.event.ActionListener() {
@@ -606,7 +606,7 @@ public class formCliente extends javax.swing.JFrame {
             }
         });
 
-        btnnuevo.setIcon(new javax.swing.ImageIcon("/Users/alexndr/GitHub/FashionProject_INFO264_2017/FashionProject2017_(GIT)/files/add.png")); // NOI18N
+        btnnuevo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Files/add.png"))); // NOI18N
         btnnuevo.setText("Nuevo Cliente");
         btnnuevo.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btnnuevo.addActionListener(new java.awt.event.ActionListener() {
@@ -615,7 +615,7 @@ public class formCliente extends javax.swing.JFrame {
             }
         });
 
-        btneliminar.setIcon(new javax.swing.ImageIcon("/Users/alexndr/GitHub/FashionProject_INFO264_2017/FashionProject2017_(GIT)/files/eliminar.png")); // NOI18N
+        btneliminar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Files/eliminar.png"))); // NOI18N
         btneliminar.setText("Eliminar Cliente");
         btneliminar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btneliminar.addActionListener(new java.awt.event.ActionListener() {
